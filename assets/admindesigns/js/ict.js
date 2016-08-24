822346b1eaebb5273c07e79456dd3133417f8828
quanict <quanict@users.noreply.github.com>
2016-08-24 16:09:00 +0700

    add remove img actions

--- assets/admindesigns/js/ict.js
+++ assets/admindesigns/js/ict.js
@@ -56,6 +56,7 @@ jQuery(document).ready(function() {
 		form.upload_img( $(this).attr('uri'),$(this).attr('inname'),$(this).attr('dir'));
 
 	});
+	form.remove_img();
 
 //	jQuery('div.imgs_add').trigger("click");
 
@@ -225,12 +226,20 @@ var form = {
 		            		$('<div class="fileupload-preview thumbnail col-sm-6" ><img src="'+data.f+'" ><input type="hidden" name="'+name+'[]" value="'+data.f+'" ></div>').insertBefore('div.imgs_add');
 
 		            	}
+		            	form.remove_img();
 		            }).complete(function(){form.ajaxing=false;});
 
 			}
 
 		});
 
+	},
+	remove_img :function(){
+		var imgs = jQuery('.fileupload-preview.thumbnail  img');
+		imgs.click(function(){
+			var div = $(this).parent('.fileupload-preview.thumbnail ');
+			if( div.length ) div.remove();
+		})
 	},
 };
 
