ee7cbef67508ef49c67020a56a931d4c8a355428
quanict <hongquan2712@gmail.com>
2017-03-28 18:06:02 +0700

    no message

--- modules/layouts/config/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-# Load the template library when the spark is loaded
-$autoload['libraries'] = array('template');
-$autoload['libraries'][] = 'smarty';
-
-$autoload['helper'] = array('template','messages','ci_smarty_function','assets');
--- modules/layouts/helpers/template_helper.php
+++
@@ -1,6 +0,0 @@
-<?php
-if ( !function_exists('temp_view')) {
-    function temp_view($view, $vars = array(), $return = FALSE){
-        get_instance()->template->build($view,$vars);
-    }
-}
