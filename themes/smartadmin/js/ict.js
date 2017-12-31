d547d3d860e131fc70058b25ce97c532d792e178
quanict <hongquan2712@gmail.com>
2017-12-31 15:14:29 +0700

    update

--- themes/smartadmin/js/ict.js
+++ themes/smartadmin/js/ict.js
@@ -3,24 +3,7 @@
  */
 
 $( document ).ready(function() {
-    // $('.summernote').each(function () {
-		// var target = $(this);
-		// if( target.is('textarea') ){
-		//     target.summernote({
-		// 	onKeyup: function (e) {
-		// 	    target.val($(this).code());
-		// 	    target.change(); //To update any action binded on the control
-		// 	}
-		//     });
-		// }
-    //
-    // });
-
-    //CKEDITOR.replaceClass = 'ckeditor';
-
 	imgupload.ready();
-
-
 });
 
 var imgupload = {
